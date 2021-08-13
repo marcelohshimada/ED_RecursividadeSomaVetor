2b3cef413598af977aa088bfa0c3ab43615f2e82
Hideki <Hideki@192.168.0.15>
2021-08-12 23:17:11 -0300

    Primeiro commit

---
+++ src/controller/VetorController.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class VetorController {
+
+	public VetorController() {
+		super();
+	}
+
+	public int somaVetor(int[] vetor, int tamanho) {
+//		Condição de parada
+		if (tamanho == 0) {
+			return 0;
+		} else {
+			int ultimaPosicao = tamanho - 1;
+			int valor = vetor[ultimaPosicao];
+			tamanho--;
+			return valor + somaVetor(vetor, tamanho);
+
+//			Opção direta e simplificada
+//			return vetor[tamanho - 1] + somaVetor(vetor, tamanho - 1);
+		}
+	}
+}
